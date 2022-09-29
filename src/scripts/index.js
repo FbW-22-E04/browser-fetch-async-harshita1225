61b1b84c5265bd64fe2f0bbfa5b333dc0204c300
harshita <harshita1225@gmail.com>
2022-09-29 16:17:52 +0200

    task completed

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,24 +1,54 @@
 import "../styles/main.scss";
-import "babel-polyfill"
+import "babel-polyfill";
+import { Button } from "bootstrap";
+//import { resolve } from "path/posix.js";
+//import { rejects } from "assert.js";
 
 const modal = document.getElementById("myModal");
+const button = document.querySelector("button");
+
 let promiseOfModal = new Promise(function (resolve) {
-    window.setTimeout(function () {
-        resolve(modal)
-    }, (1000 * 60));
+  window.setTimeout(function () {
+    resolve(modal);
+  }, 1000 * 3);
 });
 
-promiseOfModal.then(function(val) {
+let promiseButton = new Promise(function (resolve) {
+  button.addEventListener("animationend", function () {
+    alert(`Continue to subscribe!!`);
+    button.style.backgroundColor = "cyan";
+  });
+});
+
+const asyncFunc = async () => {
+  try {
+    const promiseResolve = await promiseOfModal;
     console.log("User has been on the page for 60 seconds");
-    val.style.display = "block";
-})
+    modal.style.display = "block";
+  } catch (error) {
+    console.error(error.message);
+  }
 
+  try {
+    const secondPromise = await promiseButton;
+    console.log("animation complte");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+asyncFunc();
+
+/*promiseOfModal.then(function (val) {
+  console.log("User has been on the page for 60 seconds");
+  val.style.display = "block";
+});
+*/
 modal.addEventListener("click", (e) => {
-    switch(e.target.className) {
-        case "close":
-        case "modal":
-        modal.style.display = "none"
-        break;
-    }
-})
-
+  switch (e.target.className) {
+    case "close":
+    case "modal":
+      modal.style.display = "none";
+      break;
+  }
+});
